39d358262bf5ba9ba0b37f0aa4a75585b1a69672
Bapuch <43609639+Bapuch@users.noreply.github.com>
2018-10-08 22:22:05 +0200

    last modification to clean up 05

--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,14 +1,11 @@
-#write your code here
 def time_string(t)
-=begin
-# convertir en float pour que ça marche
+    Time.at(t).utc.strftime("%H:%M:%S")
+end
+
+=begin version longue
+        t=t.to_f
         h = (t/(3600)).floor
         m = ((t/(3600)-h)*60).floor
         s = (((t/(3600)-h)*60)-m)*60
-        puts h
-        puts m
-        puts s
         "%02i:%02i:%02i" %[h,m,s]
 =end
-    Time.at(t).utc.strftime("%H:%M:%S")
-end
