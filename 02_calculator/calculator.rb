25d7b9cf0e3e68372be33f341eb53380471c135a
papuch <ngm.etcetera@gmail.com>
2018-10-08 14:46:01 +0200

    Add all exercises done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+def add(a,b)
+    a+b
+end
+
+def subtract(a,b)
+    a-b
+end
+
+def sum(arr)
+    arr.sum
+    #also works with one either one of the below methods:
+        #arr.inject(0, :+)
+        #arr.reduce(0,:+)
+end
+
+def multiply(a,b)
+    a * b
+end
+
+def power(a,b)
+    a**b
+end
+
+def factorial(n)
+    x=1
+    (n==0)? x=1 : ((1).upto(n) {|i| x*=i})
+    x
+end
--- 05_timer/timer.rb
+++ 05_timer/timer.rb
@@ -1,1 +1,14 @@
 #write your code here
+def time_string(t)
+=begin
+# convertir en float pour que ça marche
+        h = (t/(3600)).floor
+        m = ((t/(3600)-h)*60).floor
+        s = (((t/(3600)-h)*60)-m)*60
+        puts h
+        puts m
+        puts s
+        "%02i:%02i:%02i" %[h,m,s]
+=end
+    Time.at(t).utc.strftime("%H:%M:%S")
+end
